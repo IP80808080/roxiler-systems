eb8cf9d58a52a0d17102adce789763d8fefb5553
Kamlesh Kasambe <94137507+IP80808080@users.noreply.github.com>
2024-10-10 20:35:51 +0530

    route.ts

--- app/api/transcations/category/route.ts
+++ app/api/transcations/category/route.ts
@@ -1,3 +1,5 @@
+// This Project is originally created by Kamlesh Shrikant Kasambe, Proof of Work
+
 import connectDB from "@/lib/db";
 import Transaction from "@/models/Transcation";
 import { NextRequest, NextResponse } from "next/server";
